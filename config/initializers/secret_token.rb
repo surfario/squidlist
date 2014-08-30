cc5464b8f3942fc2abf021b64bb718c32caac8d3
Ari Olmos <ari.olmos@gmail.com>
2014-08-30 13:17:35 -0700

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Squidlist::Application.config.secret_key_base = '1d76b1ecd7f117d1b3d8bfd38e57c25a00ccee72f6e35e477e307e240beb25e36fc4f261033ce9642e190ad12601a9f5768b84fb942a8436f63f70929d4783e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Squidlist::Application.config.session_store :cookie_store, key: '_squidlist_session'
