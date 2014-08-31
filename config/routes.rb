cb2f2536c59e73efc2cf141029a066f07cb5f7e9
Ari Olmos <ari.olmos@gmail.com>
2014-08-31 11:53:26 -0700

    devise, welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Squidlist::Application.routes.draw do
+  devise_for :users
+  get "welcome/index"
+  get "welcome/about"
+
+  root to: 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
