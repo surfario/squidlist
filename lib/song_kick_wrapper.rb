85ba315641aec43e1c19b82390b26159089897b9
Ari Olmos <ari.olmos@gmail.com>
2014-09-17 13:16:12 -0700

    get concerts from songkick, follow surf breaks

--- lib/song_kick_wrapper.rb
+++ lib/song_kick_wrapper.rb
@@ -1,14 +1,34 @@
 class SongKickWrapper
-  include HTTParty
+  # include HTTParty
+  require 'songkickr'
 
-  API_KEY = 'DYk7diydq1KoCzS0'
+  # API_KEY = 'DYk7diydq1KoCzS0'
+  # username = 'surfario'
+
+  def get_results
+    # username = user.songkick_name
+    remote = Songkickr::Remote.new 'DYk7diydq1KoCzS0' 
+    results = remote.users_tracked_events_calendar('surfario')
+    
+    concerts = []
+    results.results.each do |r|
+      concert = {event: r.event.display_name, venue: r.event.venue.display_name, date: r.event.start}
+      concerts << concert  
+    end
+    
+    puts concerts  
+  end  
+
+  
 
   # song = SongKickWrapper.new
   # song.get_concerts
 
-  def get_concerts
-    username = 'surfario'
-    response = HTTParty.get("http://api.songkick.com/api/3.0/users/#{username}/calendar.json?reason=tracked_artist&apikey=#{API_KEY}")  
-  end
+  # def get_concerts
+  #   username = 'surfario'
+  #   response = HTTParty.get("http://api.songkick.com/api/3.0/users/#{username}/calendar.json?reason=tracked_artist&apikey=#{API_KEY}")  
+    
+  #   concerts = {concert: response["resultsPage"]["results"]["calendarEntry"]["reason"]["trackedArtist"]}
+  # end
 
 end  
