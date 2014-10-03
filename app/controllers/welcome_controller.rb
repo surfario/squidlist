7e1a24a3f3045b6df11d43e97781c47605b81368
Ari Olmos <ari.olmos@gmail.com>
2014-10-02 21:30:30 -0700

    updated welcome view, created paritals

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,12 @@ class WelcomeController < ApplicationController
   def index
     @surf_breaks = current_user && current_user.surf_breaks.any? ? current_user.surf_breaks : SurfBreak.all
     @forecasts = @surf_breaks.collect(&:forecasts).flatten
-    @concerts = current_user && current_user.concerts.any? ? current_user.concerts : Concert.all
+    concerts = current_user && current_user.concerts.any? ? current_user.concerts : Concert.all
+    today = Date.today
+    Rails.logger.info ">>>> today: #{today.inspect}"
+    Rails.logger.info ">>>>> concerts: #{concerts.inspect}"
+    @concerts = concerts.select { |concert| concert.date >= today && concert.date < (today + 7.days)}
+    Rails.logger.info ">>>>>> concerts: #{@concerts.inspect}"
     @artists = Artist.all
 
     @all_events = @forecasts + @concerts
