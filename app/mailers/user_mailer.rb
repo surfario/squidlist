b8071e2f3a628ec7a8cac8b47de6262e21b76fee
Ari Olmos <ari.olmos@gmail.com>
2014-10-05 18:01:06 -0700

    weekly email setup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,19 @@ class UserMailer < ActionMailer::Base
     @url = 'http://example.com/login'
     mail(to: @user.email, subject: 'Welcome to Squidlist!')
   end  
+
+  def weekly_email(user)
+    @surf_breaks = user.surf_breaks 
+    @forecasts = @surf_breaks.collect(&:forecasts).flatten
+    concerts = user.concerts 
+    today = Date.today
+    @concerts = concerts.select { |concert| concert.date >= today && concert.date < (today + 7.days)}
+    @artists = Artist.all
+
+    # @all_events = @forecasts + @concerts
+    # @all_events.flatten.sort_by &:date
+    @user = user
+    @url = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Your Squidlist Events for the week of #{today}')
+  end  
 end
