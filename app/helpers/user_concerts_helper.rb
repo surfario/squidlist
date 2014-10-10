386e0c4ca8df946d95ed148ef3f133c93c887be0
Ari Olmos <ari.olmos@gmail.com>
2014-10-09 17:45:27 -0700

    fixing unfollow concerts

--- app/helpers/user_concerts_helper.rb
+++ app/helpers/user_concerts_helper.rb
@@ -1,2 +1,6 @@
 module UserConcertsHelper
+   def get_user_concert_for(concert_id)
+    Rails.logger.info(">>>>>> #{concert_id.inspect}")
+    current_user.user_concerts.where(concert_id: concert_id).first
+  end
 end
