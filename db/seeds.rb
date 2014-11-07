ed892b695561b532d869f5fbc731df0f9c077393
Ari Olmos <ari.olmos@gmail.com>
2014-11-07 11:52:54 -0800

    edited seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -96,7 +96,6 @@ SurfBreak.create(
 magic = MagicSeaweedWrapper.new
 magic.get_data
 
-end
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
