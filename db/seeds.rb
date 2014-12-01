20d617cf8165fae5d73b36c4f838c8331cf87074
Ari Olmos <ari.olmos@gmail.com>
2014-12-01 13:25:39 -0800

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,48 +20,56 @@ SurfBreak.create(
   name: 'Ocean Beach', 
   mslink: 'http://magicseaweed.com/Ocean-Beach-Surf-Report/255/', 
   spot_id: '255'
+  surf_break_location_id: '1'
 )
 
 SurfBreak.create(
   name: 'Waddell Creek', 
   mslink: 'http://magicseaweed.com/Waddell-Creek-Surf-Report/3742/', 
   spot_id: '3742'
+  surf_break_location_id: '1'
 )
 
 SurfBreak.create(
   name: 'Steamer Lane', 
   mslink: 'http://magicseaweed.com/Steamer-Lane-Surf-Report/163/', 
   spot_id: '163'
+  surf_break_location_id: '1'
 )
 
 SurfBreak.create(
   name: 'Pacifica', 
   mslink: 'http://magicseaweed.com/Linda-Mar-Pacifica-Surf-Report/819/', 
   spot_id: '819'
+  surf_break_location_id: '1'
 )
 
 SurfBreak.create(
   name: 'Bolinas', 
   mslink: 'http://magicseaweed.com/Bolinas-Surf-Report/4221/', 
   spot_id: '4221'
+  surf_break_location_id: '1'
 )
 
 SurfBreak.create(
   name: 'Princeton Jetty', 
   mslink: 'http://magicseaweed.com/Princeton-Jetty-Surf-Report/3679/', 
   spot_id: '3679'
+  surf_break_location_id: '1'
 )
 
 SurfBreak.create(
   name: 'Pleasure Point', 
   mslink: 'http://magicseaweed.com/Pleasure-Point-Surf-Report/644/', 
   spot_id: '644'
+  surf_break_location_id: '1'
 )
 
 SurfBreak.create(
   name: 'Four Mile', 
   mslink: 'http://magicseaweed.com/Four-Mile-Surf-Report/257/', 
   spot_id: '257'
+  surf_break_location_id: '1'
 )
 
 # Favorite San Diego Area Surf Breaks
@@ -70,36 +78,42 @@ SurfBreak.create(
   name: 'Ocean Beach (SD)', 
   mslink: 'http://magicseaweed.com/Ocean-beach-Surf-Report/4212/', 
   spot_id: '4212'
+  surf_break_location_id: '2'
 )
 
 SurfBreak.create(
   name: 'Scripps', 
   mslink: 'http://magicseaweed.com/Scripps-Pier-La-Jolla-Surf-Report/296/', 
   spot_id: '296'
+  surf_break_location_id: '2'
 )
 
 SurfBreak.create(
   name: 'Sunset Cliffs', 
   mslink: 'http://magicseaweed.com/Sunset-Cliffs-Surf-Report/4211/', 
   spot_id: '4211'
+  surf_break_location_id: '2'
 )
 
 SurfBreak.create(
   name: 'Blacks', 
   mslink: 'http://magicseaweed.com/Torrey-Pines-Blacks-Beach-Surf-Report/295/', 
   spot_id: '295'
+  surf_break_location_id: '2'
 )
 
 SurfBreak.create(
   name: 'Windansea', 
   mslink: 'http://magicseaweed.com/Windansea-Beach-Surf-Report/4214/', 
   spot_id: '4214'
+  surf_break_location_id: '2'
 )
 
 SurfBreak.create(
   name: 'Trestles', 
   mslink: 'http://magicseaweed.com/Trestles-Surf-Report/291/', 
   spot_id: '291'
+  surf_break_location_id: '2'
 )
 
 magic = MagicSeaweedWrapper.new
