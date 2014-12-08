e621a1a6c53d243d431f95ce96638d72271bbb6a
Ari Olmos <ari.olmos@gmail.com>
2014-12-08 10:05:11 -0800

    udpated footer, worked on unfollow surfbreak js, added google images api to pull artist pic

--- app/controllers/user_surf_breaks_controller.rb
+++ app/controllers/user_surf_breaks_controller.rb
@@ -30,9 +30,11 @@ class UserSurfBreaksController < ApplicationController
   def destroy
     @user_surf_break = UserSurfBreak.find(params[:id])
     if @user_surf_break.destroy
-      redirect_to :root
-    else
-      #whatever
+      # nothing
     end
+    respond_to do |format|
+      format.html { redirect_to :root }
+      format.js
+    end 
   end  
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,7 +2,23 @@ class Artist < ActiveRecord::Base
   has_many :concerts
     mount_uploader :artistpic, ArtistpicUploader
 
+  after_create :get_artist_pic_link, if: :artistpic_image?
+
   def artistpic_image
-    self.artistpic.blank? ? 'default_concert.jpg' : self.artistpic
+  #  self.artistpic.blank? ? 'default_concert.jpg' : self.artistpic
   end
+
+  private
+
+  def artistpic_image?
+    self.artistpic.blank?
+  end
+  
+  def get_artist_pic_link
+    suckr = ImageSuckr::GoogleSuckr.new
+    self.update(link: suckr.get_image_url({"q" => "#{name}"}))
+  end  
+
 end
+
+# Is there a good way to show the artistpic in my views (when it's avialable), otherwise, show the link based image?
